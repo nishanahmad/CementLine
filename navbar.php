eb346eee9085824fa80df261008fb607c4f691f6
nishan <nishanahmad@gmail.com>
2020-12-21 16:29:12 +0530

    Menu

--- navbar.php
+++ navbar.php
@@ -288,7 +288,7 @@ a {
 	<div class="mobile-bottom-nav__item">
 		<div class="mobile-bottom-nav__item-content" onclick="javascript:location.href='https://menu.kannurzone.com/home.php'">
 			<i class="fas fa-th-large"></i>
-			Apps
+			CL
 		</div>		
 	</div>	
 </nav>
