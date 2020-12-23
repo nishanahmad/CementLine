f528156181b4ee4e0b90c6529c69b21a675bf697
nishan <nishanahmad@gmail.com>
2020-12-23 16:20:08 +0530

    UI Fixes

--- sales/list.php
+++ sales/list.php
@@ -58,6 +58,7 @@ if(isset($_SESSION["user_name"]))
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="../css/styles.css" rel="stylesheet" type="text/css">
+		<link href="../css/navbarMobile.css" media="screen and (max-device-width: 768px)" rel="stylesheet" type="text/css">
 		<link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" rel="stylesheet" type="text/css">
 		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
